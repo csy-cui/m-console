8f9d925defb2b535cf66c4d5fbb51638b504a9e8
Your Name <you@example.com>
2024-08-21 18:01:57 +0800

    主题配置

--- src/view/container.svelte
+++ src/view/container.svelte
@@ -1,6 +1,8 @@
+
 <script>
     import { setContext } from "svelte";
     import Button from "./button.svelte";
+    import Theme from "./theme/theme.svelte";
 
     export let NetworkStore;
     export let LogStore;
@@ -9,9 +11,38 @@
     export const logStore = setContext("LogStore", LogStore);
     export const storageStore = setContext("StorageStore", StorageStore);
 
-    // console.log(NetworkStore,LogStore,StorageStore)
+    let theme = "dark";
+
+    function renderPanle(node){
+        node.classList.add(theme == 'light' ? 'm_light_theme_var' : 'm_dark_theme_var')
+    }
+    
 </script>
+<Theme bind:theme={theme}/>
 
 <Button></Button>
 
-<div></div>
+<div use:renderPanle id="_mConsole_panle" class="m-panle">
+    <div class="m-panle-content"></div>
+</div>
+
+<style scoped>
+
+    .m-panle {
+        position: fixed;
+        z-index: 99999;
+        background-color: rgba(0, 0, 0, 0.4);
+        width: 100vw;
+        height: 100vh;
+        top: 0;
+        left: 0;
+    }
+    .m-panle-content {
+        width: 100%;
+        height: 80%;
+        background: var(--panle-background-color-light);
+        position: absolute;
+        bottom: 0;
+        left: 0;
+    }
+</style>
---
+++ src/view/theme/theme.svelte
@@ -0,0 +1,18 @@
+
+<script>
+    export let theme;
+</script>
+
+{#if theme == "dark"}
+    <style>
+        .m_dark_theme_var {
+            --panle-background-color-light: #333;
+        }
+    </style>
+{:else}
+    <style>
+        .m_light_theme_var {
+            --panle-background-color-light: #f2f2f2;
+        }
+    </style>
+{/if}
